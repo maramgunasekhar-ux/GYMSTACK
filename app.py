255c0392eec134d35011a86eefbcab2baf188ed5
Maram Guna Sekhar <maramgunasekhar@gmail.com>
2026-09-07 13:21:53 +0530

    Configure secure MySQL connection

--- app.py
+++ app.py
@@ -24,9 +24,11 @@ def allowed_file(filename):
 # Connect to MySQL
 db = mysql.connector.connect(
     host=config.MYSQL_HOST,
+    port=config.MYSQL_PORT,
     user=config.MYSQL_USER,
     password=config.MYSQL_PASSWORD,
-    database=config.MYSQL_DATABASE
+    database=config.MYSQL_DATABASE,
+    ssl_disabled=False
 )
 @app.route("/login", methods=["GET", "POST"])
 def login():
