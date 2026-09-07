03618a8b4b048f057374d37ebffdd2fb32e561eb
Maram Guna Sekhar <maramgunasekhar@gmail.com>
2026-09-07 13:24:55 +0530

    Update cloud database configuration

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
